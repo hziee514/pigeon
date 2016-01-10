deab43c7831bd7856cd9cc70e07bcb4a6e75c74b
hziee514 <46084337@qq.com>
2016-01-10 20:19:42 +0800

    Revert "init1"
    This reverts commit a6be54a4e6d5cc266b5b74935d117d9f6a48b489.

--- app/src/main/java/wrh/pigeon/MyExpandableListActivity.java
+++
@@ -1,9 +0,0 @@
-package wrh.pigeon;
-
-import android.app.ExpandableListActivity;
-
-/**
- * Created by Administrator on 2016/1/9.
- */
-public class MyExpandableListActivity extends ExpandableListActivity {
-}
